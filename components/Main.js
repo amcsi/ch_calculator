291a57e3c9e776bda5d01f32e98983cc04041cb0
Attila Szeremi <attila@szeremi.org>
2018-04-23 18:32:21 +0200

    Include my name on the bottom of the app

--- components/Main.js
+++ components/Main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ChToGrams from './ChToGrams';
 import Swiper from 'react-native-swiper';
 import GramsToCh from './GramsToCh';
+import { Linking, TouchableOpacity, View } from 'react-native';
+import Text from './common/Text';
 
 /** @class */
 class Main extends React.Component {
@@ -31,27 +33,43 @@ class Main extends React.Component {
 
   render() {
     return (
-      <Swiper loop={false} style={styles.container} activeDotColor="#249081">
-        <ChToGrams
-          chPer100G={this.state.chPer100G}
-          chPerMeal={this.state.chPerMeal}
-          onChPer100GramChange={this.onChPer100GramChange}
-          onChPerMealChange={this.onChPerMealChange}
-        />
-        <GramsToCh
-          chPer100G={this.state.chPer100G}
-          grams={this.state.grams}
-          onChPer100GramChange={this.onChPer100GramChange}
-          onGramsChange={this.onGramsChange}
-        />
-      </Swiper>
+      <View style={styles.container}>
+        <Swiper loop={false} activeDotColor="#249081" style={{ flex: 1 }}>
+          <ChToGrams
+            chPer100G={this.state.chPer100G}
+            chPerMeal={this.state.chPerMeal}
+            onChPer100GramChange={this.onChPer100GramChange}
+            onChPerMealChange={this.onChPerMealChange}
+          />
+          <GramsToCh
+            chPer100G={this.state.chPer100G}
+            grams={this.state.grams}
+            onChPer100GramChange={this.onChPer100GramChange}
+            onGramsChange={this.onGramsChange}
+          />
+        </Swiper>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <Text style={styles.credits}>
+            Készítette:
+          </Text>
+          <Text> </Text>
+          <TouchableOpacity onPress={() => Linking.openURL('http://www.szeremi.org/')}>
+            <Text>Szerémi Attila</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
 const styles = {
   container: {
+    flex: 1,
+    display: 'flex',
     backgroundColor: '#00695C',
+  },
+  credits: {
+    textAlign: 'center',
   },
 };
 
